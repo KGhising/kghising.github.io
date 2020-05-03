94c5f68fe81ae39912a8aed0cf129e0fec02125a
Kishor Ghising <bhfty99@gmail.com>
2020-05-03 13:30:19 +0545

    New changes

--- script/script.js
+++ script/script.js
@@ -14,19 +14,3 @@ $(document).ready(function() {
         $('html, body').animate({ scrollTop : 0},800);
     });
 });
-
-//form send
-
-// function sendEmail() {
-// 	Email.send({
-// 	Host: "smtp.gmail.com",
-// 	Username : "<sender’s email address>",
-// 	Password : "<email password>",
-// 	To : '<recipient’s email address>',
-// 	From : "<sender’s email address>",
-// 	Subject : "<email subject>",
-// 	Body : "<email body>",
-// 	}).then(
-// 		message => alert("mail sent successfully")
-// 	);
-// }
